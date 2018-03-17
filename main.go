98fcce5f683b12dcc1db46695e84b6e0b9995fff
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2018-03-17 16:10:55 +0900

    :up: Use upstream godotenv

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/sachaos/godotenv"
+	"github.com/joho/godotenv"
 	"github.com/urfave/cli"
 )
 

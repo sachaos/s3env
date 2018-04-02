009a6cdbd4cbcd657c36a898f7a7cf4e10810afb
Takumasa Sakao <takumasa.sakao@aktsk.jp>
2018-04-02 13:02:24 +0900

    :bug: Avoid to parsed args by urfave/cli on run command

--- run.go
+++ run.go
@@ -3,16 +3,13 @@ package main
 import (
 	"os"
 	"os/exec"
-
-	"github.com/urfave/cli"
 )
 
-func CmdRun(c *cli.Context) (err error) {
+func CmdRun(args []string) (err error) {
 	if err = LoadS3(); err != nil {
 		return
 	}
 
-	args := c.Args()
 	cmd := exec.Command(args[0], args[1:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout

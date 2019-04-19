c5084a1fe97d67d8b61f7566ef1281a71237db09
Takumasa Sakao <sakataku7@gmail.com>
2019-04-19 19:28:38 +0900

    Use exec

--- run.go
+++ run.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 	"os/exec"
-	"os/signal"
+	"syscall"
 )
 
 func CmdRun(args []string) (err error) {
@@ -11,26 +11,10 @@ func CmdRun(args []string) (err error) {
 		return
 	}
 
-	cmd := exec.Command(args[0], args[1:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals)
-
-	err = cmd.Start()
+	path, err := exec.LookPath(args[0])
 	if err != nil {
 		return err
 	}
 
-	go func() {
-		s := <- signals
-		err := cmd.Process.Signal(s)
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	return cmd.Wait()
+	return syscall.Exec(path, args, os.Environ())
 }
